69f0f4ca697f9c47ec11812a7d2d0868737b3639
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-11-05 12:53:08 +0600

    MainLayout updated

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,19 @@
-
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Shared/Navbar/Navbar";
+import Footer from "../components/Shared/Footer/Footer";
 
 const MainLayout = () => {
     return (
         <div>
-            Main Layout
+            <div className="max-w-screen-xl mx-auto min-h-screen px-7 md:px-16 lg:px-16 py-3 mb-7">
+                <Navbar></Navbar>
+                <Outlet></Outlet>
+            </div>
+            <div className="bg-black">
+                <div className="max-w-screen-xl mx-auto px-7 md:px-16 lg:px-9 py-3">
+                    <Footer></Footer>
+                </div>
+            </div>
         </div>
     );
 };
