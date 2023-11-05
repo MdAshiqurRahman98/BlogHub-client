aab873dbdf838b1529426ede9f2b45030a992f70
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-11-05 12:03:04 +0600

    PrivateRoute developed

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,11 +1,25 @@
+import { Navigate, useLocation } from "react-router-dom";
+import PropTypes from 'prop-types';
+import useAuth from './../hooks/useAuth';
 
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useAuth();
+    const location = useLocation();
+    // console.log(location.pathname);
 
-const PrivateRoute = () => {
-    return (
-        <div>
-            Private Route
-        </div>
-    );
+    if (loading) {
+        return <span className="loading loading-dots loading-lg"></span>;
+    }
+
+    if (user) {
+        return children;
+    }
+
+    return <Navigate state={location.pathname} to="/login" replace></Navigate>;
 };
 
 export default PrivateRoute;
+
+PrivateRoute.propTypes = {
+    children: PropTypes.node
+}
