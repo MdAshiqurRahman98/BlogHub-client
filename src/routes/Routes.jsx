1a2a80f0450c983dd04d63e4cc8e30ccd2f806b6
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-11-05 13:40:53 +0600

    routes updated

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,6 +4,10 @@ import ErrorPage from './../pages/ErrorPage/ErrorPage';
 import Home from "../pages/Home/Home/Home";
 import Login from './../pages/Login/Login';
 import Register from './../pages/Register/Register';
+import Wishlist from "../pages/Wishlist/Wishlist";
+import FeaturedBlogs from "../pages/FeaturedBlogs/FeaturedBlogs";
+import AllBlogs from "../pages/AllBlogs/AllBlogs";
+import AddBlog from "../pages/AddBlog/AddBlog";
 
 const router = createBrowserRouter([
     {
@@ -14,6 +18,22 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/add-blog',
+                element: <AddBlog></AddBlog>
+            },
+            {
+                path: '/all-blogs',
+                element: <AllBlogs></AllBlogs>
+            },
+            {
+                path: '/featured-blogs',
+                element: <FeaturedBlogs></FeaturedBlogs>
+            },
+            {
+                path: '/wishlist',
+                element: <Wishlist></Wishlist>
             },
             {
                 path: '/login',
