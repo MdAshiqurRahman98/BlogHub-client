44537348f33aa66c03699fe611091a5a618f4748
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-11-06 12:48:08 +0600

    banner added

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -2,8 +2,9 @@
 
 const Banner = () => {
     return (
-        <div>
-            Banner
+        <div className="flex flex-col-reverse md:flex-row justify-evenly items-center mt-9 md:mt-28 mb-1">
+            <h1 className="text-3xl md:text-5xl font-bold text-left my-5">Explore, Discover, and Share: Your Ultimate Blogging Platform</h1>
+            <figure><img className="w-[130px] md:w-[390px]" src="https://i.ibb.co/r3P0x4d/blog-logo.png" alt="Logo" /></figure>
         </div>
     );
 };
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,9 @@
-
+import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className="text-center">
+            <Banner></Banner>
         </div>
     );
 };
