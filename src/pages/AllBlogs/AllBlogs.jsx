0b4bdf42435535a12c0cc5c5f953bbe582b9a295
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-11-08 00:56:15 +0600

    react-table-library installed

--- src/pages/AllBlogs/AllBlogs.jsx
+++ src/pages/AllBlogs/AllBlogs.jsx
@@ -1,12 +1,52 @@
+import { useEffect, useState } from "react";
 import Blogs from "../../components/Blogs/Blogs";
+import axios from "axios";
 
 const AllBlogs = () => {
+    const [categoryValue, setCategoryValue] = useState('');
+    const [search, setSearch] = useState('');
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        axios.get(`http://localhost:5000/all-blogs/search?search=${search}`)
+            .then(res => {
+                setBlogs(res.data);
+            })
+    }, [search]);
+
+    const handleCategory = event => {
+        console.log(event.target.value);
+        setCategoryValue(event.target.value);
+    };
+
+    const handleSearch = event => {
+        event.preventDefault();
+
+        const form = event.target;
+        const searchText = form.search.value;
+        setSearch(searchText);
+
+        if (blogs.length) {
+            const blog = blogs.find(blg => blg.title === search);
+            setBlogs(blog);
+        }
+    }
+
     return (
         <div className="mb-11">
             <h3 className="text-3xl font-bold text-center mt-14 mb-9">All Blogs</h3>
-            <div className="join mb-14 flex justify-center">
-                <input className="input input-bordered join-item w-64" placeholder="Search here..." />
-                <button className="btn join-item rounded-r-xl normal-case w-24 bg-orange-500 text-white hover:bg-orange-500">Search</button>
+            <div className="mb-14 flex justify-between">
+                <select value={categoryValue} onChange={handleCategory} name="category" id="" className="select select-bordered w-full max-w-xs">
+                    <option value='Technology'>Technology</option>
+                    <option value='Food'>Food</option>
+                    <option value='Travel'>Travel</option>
+                </select>
+                <form onSubmit={handleSearch}>
+                    <div className="join">
+                        <input type="text" name="search" placeholder="Search here..." className="input input-bordered join-item w-64" />
+                        <input type="submit" value="Search" className="btn join-item rounded-r-lg normal-case w-24 bg-orange-500 text-white hover:bg-orange-500" />
+                    </div>
+                </form>
             </div>
             <Blogs></Blogs>
         </div>
