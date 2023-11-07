fd654c810db0ee1bf5ab2022596dc7a7014290f6
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-11-07 18:45:25 +0600

    All Blogs page added

--- src/pages/AllBlogs/AllBlogs.jsx
+++ src/pages/AllBlogs/AllBlogs.jsx
@@ -1,9 +1,14 @@
-
+import Blogs from "../../components/Blogs/Blogs";
 
 const AllBlogs = () => {
     return (
-        <div>
-            AllBlogs
+        <div className="mb-11">
+            <h3 className="text-3xl font-bold text-center mt-14 mb-9">All Blogs</h3>
+            <div className="join mb-14 flex justify-center">
+                <input className="input input-bordered join-item w-64" placeholder="Search here..." />
+                <button className="btn join-item rounded-r-xl normal-case w-24 bg-orange-500 text-white hover:bg-orange-500">Search</button>
+            </div>
+            <Blogs></Blogs>
         </div>
     );
 };
--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -51,7 +51,8 @@ const Wishlist = () => {
     }
 
     return (
-        <div className="mt-14 mb-11">
+        <div className="mb-11">
+            <h3 className="text-3xl font-bold text-center my-14">Wishlist</h3>
             {
                 blogs ? <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-9'>
                     {
