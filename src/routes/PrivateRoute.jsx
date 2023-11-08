2c3b7968d359d45e0d9a3a48a840c1c8b8137ebf
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-11-08 20:09:09 +0600

    react-loading-skeleton added

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,6 +1,8 @@
 import { Navigate, useLocation } from "react-router-dom";
 import PropTypes from 'prop-types';
 import useAuth from './../hooks/useAuth';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const PrivateRoute = ({ children }) => {
     const { user, loading } = useAuth();
@@ -8,7 +10,9 @@ const PrivateRoute = ({ children }) => {
     // console.log(location.pathname);
 
     if (loading) {
-        return <span className="loading loading-dots loading-lg"></span>;
+        return <span className="text-center">
+            <Skeleton count={10} />
+        </span>;
     }
 
     if (user) {
