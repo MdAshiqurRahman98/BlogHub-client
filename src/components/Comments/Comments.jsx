c39c2e4a8c5d20dc6cc5f5849e90a072395810be
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-11-19 12:14:52 +0600

    tanstack query added

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,22 +1,7 @@
-import { useEffect, useState } from "react";
-import useAxiosSecure from "../../hooks/useAxiosSecure";
-import useAuth from "../../hooks/useAuth";
 import Comment from "../Comment/Comment";
 import PropTypes from 'prop-types';
 
-const Comments = ({ _id }) => {
-    const [comments, setComments] = useState([]);
-
-    const axiosSecure = useAxiosSecure();
-    const { user } = useAuth();
-
-    // Get data to the server
-    useEffect(() => {
-        axiosSecure.get(`/all-comments?email=${user?.email}&_id=${_id}`)
-            .then(res => {
-                setComments(res.data);
-            })
-    }, [axiosSecure, user?.email, _id]);
+const Comments = ({ comments }) => {
 
     return (
         <div>
@@ -37,5 +22,5 @@ const Comments = ({ _id }) => {
 export default Comments;
 
 Comments.propTypes = {
-    _id: PropTypes.string
+    comments: PropTypes.array
 }
